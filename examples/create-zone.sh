1cda12aa2ab0acecb0af72f796bd339f868e7145
Jarle Aase <jarle@jgaa.com>
2021-03-08 18:26:54 +0200

    Added example with curl to create a soa zone

---
+++ examples/create-zone.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl -d '{"ns": [{"fqdn": "ns1"},{"fqdn": "ns2"}],"soa": {}}' -H 'Content-Type: application/json' http://127.0.0.1:8080/zone/example.com
+
+curl -d '{"a": ["127.0.0.1"]}' -H 'Content-Type: application/json' http://127.0.0.1:8080/zone/ns1.example.com
+
+curl -d '{"a": ["127.0.0.2"]}' -H 'Content-Type: application/json' http://127.0.0.1:8080/zone/ns2.example.com
